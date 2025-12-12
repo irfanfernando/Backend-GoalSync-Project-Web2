bbd044e6faddffa2e3268e742df63770b2ab4e0b
Dharmananda20 <dharmananda_2327240078@mhs.mdp.ac.id>
2025-12-12 14:16:26 +0700

    Menambahkan Avatar

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {type: String, requires: true},
     email: {type: String, requires: true, unique: true},
-    password :{type: String, requires: true}
+    password :{type: String, requires: true},
+    avatar : {type:String, default: null}
 },{
     timestamps: true
 });
