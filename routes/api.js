53b160de539595f21db69a7f25efaaf51dfc1610
Dharmananda20 <dharmananda_2327240078@mhs.mdp.ac.id>
2025-12-12 14:44:29 +0700

    Menambahkan api.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ import express from "express";
 import * as goalController from "../controllers/goalController.js"
 import * as memberController from "../controllers/memberController.js"
 import * as progressController from "../controllers/progressController.js"
+import upload from "../middleware/uploadAvatar.js";
 import * as userController from "../controllers/userController.js"
 import { authenticateTokenMiddleware } from "../middleware/authMiddleware.js"
 
@@ -28,5 +29,8 @@ api.post("/goals/:id/progress", authenticateTokenMiddleware, progressController.
 //Auth
 api.post("/signin", userController.signIn);
 api.post("/signup", userController.signUp);
+
+api.get("/me", authenticateTokenMiddleware, userController.getMe);
+api.put("/me", authenticateTokenMiddleware, upload.single("avatar"), userController.updateMe);
 
 export default api;
