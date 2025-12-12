e3c9e51a1bc22536cd32b1cfccfdc953437db62f
Dharmananda20 <dharmananda_2327240078@mhs.mdp.ac.id>
2025-12-12 14:22:25 +0700

    installed multer sharp package dan menambahkan UploadAvatar

---
+++ middleware/uploadAvatar.js
@@ -0,0 +1,12 @@
+import multer from "multer"
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+  fileFilter: (req, file, cb) => {
+    const allowed = ["image/jpeg", "image/png", "image/webp"];
+    if (allowed.includes(file.mimetype)) cb(null, true);
+    else cb(new Error("Invalid file type. Only JPEG / PNG / WEBP allowed."));
+  },
+});
+
+export default upload;
